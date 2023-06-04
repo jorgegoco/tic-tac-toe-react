33cf84a482ea9b923bbfadcf060298149a454198
jorgegoco <jorgegoco70@gmail.com>
2023-06-04 19:38:33 +0200

    Set up files, and delete unused ones

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div>
+    <div className="board-row">
+      <button type="button" className="square">1</button>
+      <button type="button" className="square">2</button>
+      <button type="button" className="square">3</button>
     </div>
-  );
-}
+    <div className="board-row">
+      <button type="button" className="square">4</button>
+      <button type="button" className="square">5</button>
+      <button type="button" className="square">6</button>
+    </div>
+    <div className="board-row">
+      <button type="button" className="square">7</button>
+      <button type="button" className="square">8</button>
+      <button type="button" className="square">9</button>
+    </div>
+  </div>
+);
 
 export default App;
