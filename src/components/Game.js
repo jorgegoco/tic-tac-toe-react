67177d56a858f398eb4eb728d4b99d29ff263275
jorgegoco <jorgegoco70@gmail.com>
2023-06-05 14:45:43 +0200

    Add basic props and funtions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Board from './Board';
 
-const Game = () => <div>Game</div>;
+const Game = () => {
+  const handleClick = () => {
+  };
+
+  const jumpTo = () => {
+  };
+
+  const renderMoves = () => {
+  };
+
+  return (
+    <Board onClick={handleClick} />
+  );
+}
 
 export default Game;
