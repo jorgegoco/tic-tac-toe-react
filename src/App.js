78e1642c2a202bd59a36a22086d551ce91339846
jorgegoco <jorgegoco70@gmail.com>
2023-06-05 14:17:26 +0200

    Create basic components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-const App = () => (
-  <div>
-    <div className="board-row">
-      <button type="button" className="square">1</button>
-      <button type="button" className="square">2</button>
-      <button type="button" className="square">3</button>
-    </div>
-    <div className="board-row">
-      <button type="button" className="square">4</button>
-      <button type="button" className="square">5</button>
-      <button type="button" className="square">6</button>
-    </div>
-    <div className="board-row">
-      <button type="button" className="square">7</button>
-      <button type="button" className="square">8</button>
-      <button type="button" className="square">9</button>
-    </div>
-  </div>
-);
+import React from 'react';
+import Game from './components/Game';
+
+const App = () => <Game />;
 
 export default App;
