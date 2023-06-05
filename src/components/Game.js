e07a6908bf757688a1f990d5b64e33954d4a02e2
jorgegoco <jorgegoco70@gmail.com>
2023-06-05 20:02:21 +0200

    Set main logic in handleClick function in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { calculateWinner } from '../helpers';
 import Board from './Board';
 
 const Game = () => {
+  const [board, setBoard] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
   const handleClick = () => {
+    const boardCopy = [...board];
+    // If user clicks an occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    // Put an X or an O in the clicked square
+    boardCopy[i] = xIsNext ? 'X' : 'O'; 
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
   };
 
   const jumpTo = () => {
@@ -12,7 +23,7 @@ const Game = () => {
   };
 
   return (
-    <Board onClick={handleClick} />
+    <Board  squares={board} onClick={handleClick} />
   );
 }
 
